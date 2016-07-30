cbedbfc784958c216f1cb956ef8500297fcd2ab3
durgesh06 <Durgesh.Kumar@cdk.com>
2016-07-30 12:32:34 +0530

    Add files via upload

---
+++ Bishop.java
@@ -0,0 +1,12 @@
+/**
+ * Created by kumardu on 7/30/2016.
+ */
+public class Bishop extends Piece{
+
+    public Bishop(boolean available, int x, int y) {
+        super(available, x, y);
+        // TODO Auto-generated constructor stub
+    }
+
+
+}
