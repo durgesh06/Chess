aeaeef0b4e89ee36ad94da3725e44990ef54c420
Himansh2294 <himanshu00238@gmail.com>
2016-07-30 12:55:48 +0530

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -3,17 +3,17 @@
  */
 public class Bishop extends Piece{
 
-    public Bishop(boolean available, int x, int y) {
-        super(available, x, y);
+    public Bishop(boolean available, int rank, int file) {
+        super(available, rank, file);
         // TODO Auto-generated constructor stub
     }
     
     @Override
-    public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
-        if(super.isValid(board, fromX, fromY, toX, toY) == false)
+    public boolean isValid(Board board, int fromRank, int fromfile, int toRank, int tofile) {
+        if(super.isValid(board, fromRank, fromFile, toRank, toFile) == false)
             return false;
 
-        if(toX - fromX == toY - fromY)
+        if(toRank - fromRank == toFile - fromFile)
             return true;
 
         return false;
