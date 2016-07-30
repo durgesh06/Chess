25f1ba39b7268c2703d2fe96fdfd9f2c47917468
Himansh2294 <himanshu00238@gmail.com>
2016-07-30 12:37:47 +0530

    included rank and file, methods - occupySquare and releaseSquare

--- Square.java
+++ Square.java
@@ -1,2 +1,27 @@
 public class Square{
+  int rank;
+    int file;
+    Piece piece;
+
+    public Spot(int rank, int file) {
+        super();
+        this.rank = rank;
+        this.file = file;
+        piece = null;
+    }
+
+    public void occupySquare(Piece piece){
+        //if piece already here, delete it, i. e. set it dead
+        if(this.piece != null)
+            this.piece.setAvailable(false);
+        //place piece here
+        this.piece = piece;
+    }
+
+    public Piece releaseSquare() {
+        Piece releasedPiece = this.piece;
+        this.piece = null;
+        return releasedPiece;
+    }
+
 }
